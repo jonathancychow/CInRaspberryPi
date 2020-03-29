68b136aa3a9a2b78e4974eadfe0b386d782aca6a
Jonathan Chow <jonathan.chow@mclaren.com>
2020-03-29 17:14:38 +0100

    Update pointer_function_example.c

--- pointer_function_example.c
+++ pointer_function_example.c
@@ -10,11 +10,14 @@ void pointer_array() {
   int* ptr2;
 
   // ptr is assigned the address of the third element
-  ptr2 = &x[2]; 
+  ptr2 = &x[0]; 
 
   printf("*ptr = %d \n", *ptr2);   // 3
   printf("*(ptr+1) = %d \n", *(ptr2+1)); // 4
-  printf("*(ptr-1) = %d", *(ptr2-1));  // 2
+
+  for (i=0; i<=5; i++){
+      printf("*ptr = %d \n", *(ptr2+i));   // 3
+  }
 
 }
 
@@ -22,13 +25,13 @@ int main()
 {
   int* p, i = 10;
   p = &i;
-  printf("line 11 *p = %d",*p);
-  printf("line 12 p = %d",p);
+  printf("line 28 *p = %d\n",*p);
+  printf("line 29 p = %d\n",p);
   addOne(p);
 
-  printf("%d", *p); // 11
+  printf("line 32 %d\n", *p); // 11
 
-  pointer_array()
+  pointer_array();
   return 0;
 }
 
